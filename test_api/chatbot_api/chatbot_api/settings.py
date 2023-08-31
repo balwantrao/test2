af4b3ea53fdda520fef5adb7f182fa9de2f94e25
balwantrao <106421117+balwantrao@users.noreply.github.com>
2023-08-31 22:39:47 +0530

    settings.py

--- test_api/chatbot_api/chatbot_api/settings.py
+++ test_api/chatbot_api/chatbot_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g75)-=qwiltkx++90k$jx=!bderiv8g6xdo_l*o0u@bft9^5^f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000']
 
 
 # Application definition
